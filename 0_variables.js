efe4c3f4acc8fc548b7a400c8adc9748687b6d34
luvtomar2005 <luvt354@gmail.com>
2025-10-14 10:45:17 +0530

    Changed file name

---
+++ 0_variables.js
@@ -0,0 +1,18 @@
+// In javascript variables and values are fundamentals that forms the basics of programming
+
+// Values -: A piece of information that a programmer works with It can be a number , name , boolean value like true/false
+
+// Variables -: It acts as a container that holds the value
+
+var my_firstName = "John";
+console.log(my_firstName);
+var _myLastName$ = "Doe"; // This is valid as variables can start with $ and _\
+console.log(_myLastName$);
+// var 123myAge = 25;
+// Variables can not start with the number
+// console.log(123myAge);
+var $cityName = "New York";
+console.log($cityName);
+// var my@Email = "john@example.com";
+// @ symbol is not allowed in variables name....
+// console.log(my@Email);
