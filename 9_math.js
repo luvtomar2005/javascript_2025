a6740a5e558a11dde9a26fe220899edb4d57d7bf
luvtomar2005 <luvt354@gmail.com>
2025-10-29 00:18:59 +0530

    Commit

--- 9_math.js
+++ 9_math.js
@@ -1,1 +1,22 @@
 // Now we learn about Math function in javascript
+// Math.round -> Round to the nearest integer.. 
+console.log(Math.round(4.5));
+
+// Math.floor() -> Always round down to the nearest integer
+console.log(Math.floor(4.9));
+
+// Math.ceil() -> Always round up to the nearest integer
+console.log(Math.ceil(4.2));
+
+
+//Math.abs -> basically the number is how much far from 0
+console.log(Math.abs(4.434342));
+console.log(Math.abs(-2));
+
+// Math.trun -> Returns the integer part of x
+console.log(Math.trunc(93.32));
+
+// Math.pow -> Return the power of x to y
+console.log(Math.pow(2,4));
+
+console.log(Math.round(Math.random() * 100));
